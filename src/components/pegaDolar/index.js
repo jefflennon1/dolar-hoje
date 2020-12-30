126a7f50109abc47d20a2b2c791a5b3668237873
jefferson lennon <jeffersonlennon1@outlook.com>
2020-12-29 21:35:05 -0300

    Um pouco do layout snedo produzido

--- src/components/pegaDolar/index.js
+++ src/components/pegaDolar/index.js
@@ -1,19 +1,44 @@
 import React, {useEffect, useState }from 'react';
 import api from '../../../services/api'
+import './style.css'
+
 
 export default function PegaDolar(){
-  const [infoDolar, setInfoDolar ] = useState([])
+  const [ dolar, setDolar] = useState([]);
+  const [ euro, setEuro ] = useState([]);
 
   useEffect(()=>{
-    api.get('/usd').then((response)=>{
-      console.log(response.data[0]);
-      // setInfoDolar([response.data])
+    api.get('/all').then((response)=>{
+      // response.data -> tem todas as moedas;
+      setDolar([response.data.USD])
+      setEuro([response.data.EUR])      
+     
     })
-  , []})
+  }, [])
 
   return(
     <>
-      {infoDolar.map( info => <h1 key={info.timestamp}>{info.codes}</h1>)}
+      <div className="divDolar divPadrao">
+          <h2>
+            {dolar.map(dolar => <div key={dolar.timestamp}>{dolar.name}</div>)}
+          </h2>
+          <div>  
+            <div>
+              Valor atual:
+              {dolar.map(dolar=> <div key={dolar.timestamp+1 }>{dolar.ask}</div>)}
+            </div>
+          </div>
+      </div>
+      
+      <div className="divEuro  divPadrao">
+          <h2>
+            {euro.map(euro => <div key={euro.timestamp}>{euro.name}</div>)}
+          </h2>
+          <div>
+            Valor hoje:
+            {euro.map(euro =><div key={euro.timestamp+1}>{euro.ask}</div>)}
+          </div>
+      </div>
     </>
   )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,15 @@ module.exports ={
           use:{
             loader: 'babel-loader',
           }
-        }
+        },
+        {
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use:[
+              {loader: 'style-loader'},
+              {loader: 'css-loader'}
+            ]
+        },
       ]
     },
     devServer:{
